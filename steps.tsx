4cf5f7f7e2695df780b8c7851f14b6ef7b3f045f
Michael Williams <michael.williams9@ticketmaster.co.uk>
2023-03-22 10:14:45 +0000

    SS: Create steps file
    - Creating a file with Steps learned in the Beginner 1 class
    
    [demo] [e2e]

---
+++ steps.tsx
@@ -0,0 +1,60 @@
+const steps = [
+  {
+    id: 1,
+    name: "Basic Steps",
+    step_count: [1, 2, 3, 5, 6, 7],
+    instruction: [
+      "Step forward with your left foot",
+      "Step on the same spot with your right foot",
+      "Step backwards to your original spot with your left foot",
+      "Step backwards with your right foot",
+      "Step on the same spot with your left foot",
+      "Step forward to your original spot with your right foot",
+    ],
+    class: "Beginners 1",
+  },
+  {
+    id: 2,
+    name: "Side Steps",
+    step_count: [1, 2, 3, 5, 6, 7],
+    instruction: [
+      "Step to the side with your left foot",
+      "Step on the same spot with your right foot",
+      "Step back to your original spot with your left foot, end with your feet together",
+      "Step to the side with your right foot",
+      "Step on the same spot with your left foot",
+      "Step on the back to your original spot with your right foot, end with your feet together",
+    ],
+    class: "Beginners 1",
+  },
+  {
+    id: 3,
+    name: "Double Forawrd",
+    step_count: [1, 2, 3, 5, 6, 7],
+    instruction: [
+      "Step forward with your left foot",
+      "Step on the same spot with your right foot",
+      "Step backwards to your original spot with your left foot",
+      "Step forward with your right foot",
+      "Step on the same spot with your left foot",
+      "Step backwards to your original spot with your right foot",
+    ],
+    class: "Beginners 1",
+  },
+  {
+    id: 4,
+    name: "Double Backwards",
+    step_count: [1, 2, 3, 5, 6, 7],
+    instruction: [
+      "Step backwards with your left foot",
+      "Step on the same spot with your right foot",
+      "Step forward to your original spot with your left foot",
+      "Step backwards with your right foot",
+      "Step on the same spot with your left foot",
+      "Step forward to your original spot with your right foot",
+    ],
+    class: "Beginners 1",
+  },
+];
+
+export default steps;
